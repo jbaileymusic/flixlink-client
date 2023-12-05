08d037402cba4bb0900fc5eaf80bed363bda194c
jbaileymusic <46546066+jbaileymusic@users.noreply.github.com>
2023-12-04 20:31:28 -0500

    Built all components with mock movie data

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
+import { MainView } from "./components/main-view/main-view";
+
 import "./index.scss";
 
 const FlixLinkApp = () => {
-    return (
-        <div className="flixlink">
-            <div>Good Morning</div>
-        </div>
-    );
+    return <MainView />;
 };
 
 const container = document.querySelector("#root");
