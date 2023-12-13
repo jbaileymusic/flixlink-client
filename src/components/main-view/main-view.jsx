5b14e2851deda39764c6ad3ac1612248340b240b
jbaileymusic <46546066+jbaileymusic@users.noreply.github.com>
2023-12-13 18:14:28 -0500

    Added Login and Signup views

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+<<<<<<< Updated upstream
   const [movies, setMovies] = useState([
     {
       id: 1,
@@ -35,6 +38,56 @@ export const MainView = () => {
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+=======
+
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [movies, setMovies] = useState([]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const [user, setUser] = useState(null);
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch("https://flexlink-11694b6f8913.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        const moviesFromApi = movies.map((movie) => {
+          return {
+            id: movie.key,
+            Title: movie.Title,
+            Description: movie.Description,
+            Genre: movie.Genre,
+            Director: movie.Director,
+            ImagePath: movie.ImagePath,
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, [token]);
+
+  
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />          
+        </>
+    );
+  }
+
+>>>>>>> Stashed changes
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -53,6 +106,7 @@ export const MainView = () => {
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
+          <button onClick={() => { setUser(null); setToken(null); localStorage.clear() }}>LOGOUT</button>
           }}
         />
       ))}
